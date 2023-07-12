56c95d442ce3cf967e868a6707c87c182c045e91
largefort <106451813+largefort@users.noreply.github.com>
2023-07-12 15:30:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,43 @@ document.addEventListener("DOMContentLoaded", function() {
   var uploadButton = document.getElementById("upload-button");
   var uploadStatus = document.getElementById("upload-status");
   var speedCounter = document.getElementById("network-speed");
-  
+  var fileInput = document.getElementById("file-input");
+
   var totalUploaded = 0;
   var startTime;
   var elapsedTime;
-  
+
   uploadButton.addEventListener("click", function() {
     if (navigator.connection && navigator.connection.downlink) {
       startTime = performance.now();
       speedCounter.textContent = "0 Mbps";
-      
-      var progressInterval = setInterval(function() {
-        if (totalUploaded >= 100) {
-          clearInterval(progressInterval);
-          uploadStatus.textContent = "Upload complete!";
-        } else {
-          totalUploaded += 10;
-          uploadStatus.textContent = "Uploading... " + totalUploaded + "%";
-          calculateSpeed();
-        }
-      }, 1000); // Adjust the interval as desired
+      fileInput.click(); // Trigger file input click event
     } else {
       uploadStatus.textContent = "Network speed cannot be determined.";
     }
   });
-  
+
+  fileInput.addEventListener("change", function() {
+    var file = fileInput.files[0];
+    var fileSize = file.size;
+    var chunkSize = 10; // MB
+
+    var numChunks = Math.ceil(fileSize / (chunkSize * 1024 * 1024));
+    var currentChunk = 0;
+
+    var uploadProgressInterval = setInterval(function() {
+      if (currentChunk >= numChunks) {
+        clearInterval(uploadProgressInterval);
+        uploadStatus.textContent = "Upload complete!";
+      } else {
+        currentChunk++;
+        totalUploaded += chunkSize;
+        uploadStatus.textContent = "Uploading... " + currentChunk + "/" + numChunks + " chunks";
+        calculateSpeed();
+      }
+    }, 1000); // Adjust the interval as desired
+  });
+
   function calculateSpeed() {
     elapsedTime = (performance.now() - startTime) / 1000; // Convert to seconds
     var speed = (totalUploaded / elapsedTime).toFixed(2); // Calculate speed in Mbps
